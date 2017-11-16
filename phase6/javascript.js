8fd06652f8f3b8077f645904f67d1ce31d4c3a2e
Koby <koby_wu@davincischools.org>
2017-11-15 16:47:23 -0800

    Update javascript.js

--- phase6/javascript.js
+++ phase6/javascript.js
@@ -2,6 +2,18 @@ function facts(e){
 	e.classList.toggle("hide");
 }
 function play(){
-       var audio = document.getElementById("audio");
+       var audio = document.getElementById("love");
+       audio.play();
+                 }
+function play(){
+       var audio = document.getElementById("dark");
+       audio.play();
+                 }
+function play(){
+       var audio = document.getElementById("young");
+       audio.play();
+                 }
+function play(){
+       var audio = document.getElementById("wings");
        audio.play();
                  }
