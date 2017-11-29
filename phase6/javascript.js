312ed4ff97f4f067d538b8aa211fd1e6f6e94d46
Koby <koby_wu@davincischools.org>
2017-11-29 14:22:32 -0800

    Update javascript.js

--- phase6/javascript.js
+++ phase6/javascript.js
@@ -1,16 +1,93 @@
 function young(){
        var audio = document.getElementById("young");
        audio.play();
-                 }
+}
 function dark(){
        var audio = document.getElementById("dark");
        audio.play();
-                 }
+}
 function love(){
        var audio = document.getElementById("love");
        audio.play();
-                 }
+}
 function wings(){
        var audio = document.getElementById("wings");
        audio.play();
-                 }
+}
+function jinfacts(){
+	document.getElementById("jin").classList.remove("jin");
+	document.getElementById("jin").classList.add("facts");
+	document.getElementById("jin").innerHTML = "Paragraph changed!";
+}
+function jinpicture(){
+	document.getElementById("jin").classList.remove("facts");
+	document.getElementById("jin").classList.add("jin");
+	document.getElementById("jin").innerHTML = "";
+}
+		 
+function rapmonfacts(){
+	document.getElementById("rapmon").classList.remove("rapmon");
+	document.getElementById("rapmon").classList.add("facts");
+	document.getElementById("rapmon").innerHTML = "Paragraph changed!";
+}
+function rapmonpicture(){
+	document.getElementById("rapmon").classList.remove("facts");
+	document.getElementById("rapmon").classList.add("rapmon");
+	document.getElementById("rapmon").innerHTML = "";
+}
+		 
+function jhopefacts(){
+	document.getElementById("jhope").classList.remove("jhope");
+	document.getElementById("jhope").classList.add("facts");
+	document.getElementById("jhope").innerHTML = "Paragraph changed!";
+}
+function jhopepicture(){
+	document.getElementById("jhope").classList.remove("facts");
+	document.getElementById("jhope").classList.add("jhope");
+	document.getElementById("jhope").innerHTML = "";
+}
+		 
+function jiminfacts(){
+	document.getElementById("jimin").classList.remove("jimin");
+	document.getElementById("jimin").classList.add("facts");
+	document.getElementById("jimin").innerHTML = "Paragraph changed!";
+}
+function jiminpicture(){
+	document.getElementById("jimin").classList.remove("facts");
+	document.getElementById("jimin").classList.add("jimin");
+	document.getElementById("jimin").innerHTML = "";
+}
+
+function jkfacts(){
+	document.getElementById("jk").classList.remove("jk");
+	document.getElementById("jk").classList.add("facts");
+	document.getElementById("jk").innerHTML = "Paragraph changed!";
+}
+function jkpicture(){
+	document.getElementById("jk").classList.remove("facts");
+	document.getElementById("jk").classList.add("jk");
+	document.getElementById("jk").innerHTML = "";
+}
+		 
+function vfacts(){
+	document.getElementById("v").classList.remove("v");
+	document.getElementById("v").classList.add("facts");
+	document.getElementById("v").innerHTML = "Paragraph changed!";
+}
+function vpicture(){
+	document.getElementById("v").classList.remove("facts");
+	document.getElementById("v").classList.add("v");
+	document.getElementById("v").innerHTML = "";
+}
+
+function sugafacts(){
+	document.getElementById("suga").classList.remove("suga");
+	document.getElementById("suga").classList.add("facts");
+	document.getElementById("suga").innerHTML = "Paragraph changed!";
+}
+function sugapicture(){
+	document.getElementById("suga").classList.remove("facts");
+	document.getElementById("suga").classList.add("suga");
+	document.getElementById("suga").innerHTML = "";
+}
+		 
