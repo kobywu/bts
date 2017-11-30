60f4eef7dc1fb41ef32a97140c5d3ecd0d5d5a28
Koby <koby_wu@davincischools.org>
2017-11-30 14:42:56 -0800

    Update javascript.js

--- phase6/javascript.js
+++ phase6/javascript.js
@@ -1,93 +1,69 @@
 function young(){
-       var audio = document.getElementById("young");
-       audio.play();
+       document.getElementById("young").play();
 }
 function dark(){
-       var audio = document.getElementById("dark");
-       audio.play();
+       document.getElementById("dark").play();
 }
 function love(){
-       var audio = document.getElementById("love");
-       audio.play();
+       document.getElementById("love").play();
 }
 function wings(){
-       var audio = document.getElementById("wings");
-       audio.play();
+       document.getElementById("wings").play();
+}
+function picture(elem){
+	elem.innerHTML="";
+	elem.classList.remove("facts");
+}
+function facts(elem){
+	elem.classList.add("facts");
 }
 function jinfacts(){
 	document.getElementById("jin").classList.remove("jin");
-	document.getElementById("jin").classList.add("facts");
-	document.getElementById("jin").innerHTML = "Paragraph changed!";
+	document.getElementById("jin").innerHTML = "Kim Seok Jin also known as Jin was born on december 4, 1992. He loves strawberries but not strawberry flavored things and Super Mario.";
 }
 function jinpicture(){
-	document.getElementById("jin").classList.remove("facts");
 	document.getElementById("jin").classList.add("jin");
-	document.getElementById("jin").innerHTML = "";
 }
-		 
 function rapmonfacts(){
 	document.getElementById("rapmon").classList.remove("rapmon");
-	document.getElementById("rapmon").classList.add("facts");
-	document.getElementById("rapmon").innerHTML = "Paragraph changed!";
+	document.getElementById("rapmon").innerHTML = "Kim Nam-Joon also known as Rap Monster is the leader of the group. He learned English by watching the show Friends and has composed or produced atleast 120 songs.";
 }
 function rapmonpicture(){
-	document.getElementById("rapmon").classList.remove("facts");
 	document.getElementById("rapmon").classList.add("rapmon");
-	document.getElementById("rapmon").innerHTML = "";
-}
-		 
+} 
 function jhopefacts(){
 	document.getElementById("jhope").classList.remove("jhope");
-	document.getElementById("jhope").classList.add("facts");
-	document.getElementById("jhope").innerHTML = "Paragraph changed!";
+	document.getElementById("jhope").innerHTML = "Jung Ho-seok also known as J-Hope was born Febraury 18, 1994. He is the mood maker and the energy vitamin of the group. He also hates snakes.";
 }
 function jhopepicture(){
-	document.getElementById("jhope").classList.remove("facts");
 	document.getElementById("jhope").classList.add("jhope");
-	document.getElementById("jhope").innerHTML = "";
-}
-		 
+}		 
 function jiminfacts(){
 	document.getElementById("jimin").classList.remove("jimin");
-	document.getElementById("jimin").classList.add("facts");
-	document.getElementById("jimin").innerHTML = "Paragraph changed!";
+	document.getElementById("jimin").innerHTML = "Park Jimin was born October 13, 1995 and is the shortest member of the group. His stage name was suppose to be Baby J or Young Kid and he was the last member to join BTS.";
 }
 function jiminpicture(){
-	document.getElementById("jimin").classList.remove("facts");
 	document.getElementById("jimin").classList.add("jimin");
-	document.getElementById("jimin").innerHTML = "";
 }
-
 function jkfacts(){
 	document.getElementById("jk").classList.remove("jk");
-	document.getElementById("jk").classList.add("facts");
-	document.getElementById("jk").innerHTML = "Paragraph changed!";
+	document.getElementById("jk").innerHTML = "Jeon Jongkook was born September 1, 1997 and is the maknae (youngest) in BTS.  He has a black belt in Taekwondo and his favorite times of the day is resting time, free time and eating time.";
 }
 function jkpicture(){
-	document.getElementById("jk").classList.remove("facts");
 	document.getElementById("jk").classList.add("jk");
-	document.getElementById("jk").innerHTML = "";
-}
-		 
+}	 
 function vfacts(){
 	document.getElementById("v").classList.remove("v");
-	document.getElementById("v").classList.add("facts");
-	document.getElementById("v").innerHTML = "Paragraph changed!";
+	document.getElementById("v").innerHTML = "Kim Taehyung also known as V was born December 30, 1995. He loves bubbles, can speak Japanese and is the pickest eater of the group.";
 }
 function vpicture(){
-	document.getElementById("v").classList.remove("facts");
 	document.getElementById("v").classList.add("v");
-	document.getElementById("v").innerHTML = "";
 }
-
 function sugafacts(){
 	document.getElementById("suga").classList.remove("suga");
-	document.getElementById("suga").classList.add("facts");
-	document.getElementById("suga").innerHTML = "Paragraph changed!";
+	document.getElementById("suga").innerHTML = "Min Yoon Gi also known as Suga was born March 9, 1993. He got that stage name because he is sweet as sugar. He loves basketball and is known for being like a granpa because he likes to whine.";
 }
 function sugapicture(){
-	document.getElementById("suga").classList.remove("facts");
 	document.getElementById("suga").classList.add("suga");
-	document.getElementById("suga").innerHTML = "";
-}
-		 
+}	 
+
