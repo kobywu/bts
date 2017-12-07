f185b7dcb7e6cae2a5b3dd6cbe133e6bab71dbc2
Koby <koby_wu@davincischools.org>
2017-12-06 19:15:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//There are 4 albums divs in my website. Within each div, there are audio files with ids named love, young, dark, and wings named after the albums (divs) they are in. 
+//There are 4 albums divs in my website. Within each div, there are audio files with ids named love, young, dark, and wings named after the albums (divs) they are which is located in the HTML. 
 function young(){ 
 	//The function of "young" happens when the user clicks on the image of the album Young Forever.  
-	//The 3 audio files that were not clicked on, will pause their audio sounds when the young album is clicked because I specifically stated that the element that has these ids's will pause their audio. 
+	//The 3 audio files that were not clicked on, will pause their audio sounds when the young album is clicked because I specifically stated that the element that has these ids's in the HTML will pause their audio. 
 	document.getElementById("love").pause();
 	document.getElementById("wings").pause();
 	document.getElementById("dark").pause();
-	//Then the audio file with the id of young(the album that I clicked on) will start play their most popular song on that album when I specifically got the young element's id of "young" and made that audio sound play.
+	//Then, the audio file with the id of young(the album that I clicked on) will start play their most popular song on that album when I specifically got the young element's id of "young" from the HTML and made that audio sound play.
     document.getElementById("young").play();
 }
 function dark(){
@@ -26,17 +26,17 @@ function wings(){
 	document.getElementById("love").pause();
     document.getElementById("wings").play();
 }
-//The 4 albums also have an effect when you hover over it. The div of the albums have the function "opacity" when mouse is over it. This will make the element that was hovered on add the class "opacity" and will change the appearance of the album.
+//The 4 albums also have an effect when you hover over it. The div of the albums have the function "opacity" when mouse is over it. This will make the element in the HTML that was hovered on add the CSS class "opacity" and will change the appearance of the album.
 function opacity(elem){
 	elem.classList.add("opacity");
 }	
-//This function is in the divs of the 4 albums on "onmouseover". When the user hovers out of the div of the album, it will take out the class opacity making the image of the album go back to the orginial picture. 
+//This function is in the divs of the 4 albums on "onmouseover" in the HTML. When the user hovers out of the div of the album, it will take out the class "opacity" from the HTMLL making the image of the album go back to the orginial picture. 
 function regular(elem){
 	elem.classList.remove("opacity");
 }	
 
 //This is the javascript for the 7 members
-//This function "picture" is in the divs of the 7 members on "onmouseout". When the user hovers out of the divs of the 7 boys, it will remove the class "facts" (the background color) and have no text shown
+//This function "picture" is in the HTML divs of the 7 members on "onmouseout". When the user hovers out of the divs of the 7 boys, it will remove the CSS class "facts" (the background color) from the HTML code and have no text shown
 function picture(elem){
 	elem.innerHTML="";
 	elem.classList.remove("facts");
@@ -45,12 +45,12 @@ function picture(elem){
 function facts(elem){
 	elem.classList.add("facts");
 }
-//The function "jinfacts" is only in the jin picture div with the id of "jin". When the user hovers over this div, the div is going to remove the class "jin" which is the picture of jin. It is also going to add text in that div.
+//The function "jinfacts" is only in the jin picture div with the id of "jin". When the user hovers over this div, the div is going to remove the CSS class "jin" which is the picture of jin in the HTML. It is also going to add text in that div.
 function jinfacts(){
 	document.getElementById("jin").classList.remove("jin");
 	document.getElementById("jin").innerHTML = "Kim Seok Jin also known as Jin was born on december 4, 1992. He loves strawberries but not strawberry flavored things, Disney Princesses and Super Mario. He is also the worst dancer in the group.";
 }
-//The function "jinpicture" is for the jin picture div on the site. The jin div has the id of "jin". This function will find the element by the id and add the class jin to that div making it change from text to a picture of him.
+//The function "jinpicture" is for the div with the picture of jin. The jin div has the id of "jin" in the HTML. This function will find the element by the id in the HTML and add the CSS class "jin" to that div making it change from text to a picture of him because of the cascade effect.
 function jinpicture(){
 	document.getElementById("jin").classList.add("jin");
 }
