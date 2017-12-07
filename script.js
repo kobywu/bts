7ab1c89b90c010e1d1f855ecf594e08d8f639cf7
Koby <koby_wu@davincischools.org>
2017-12-06 19:16:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function wings(){
 function opacity(elem){
 	elem.classList.add("opacity");
 }	
-//This function is in the divs of the 4 albums on "onmouseover" in the HTML. When the user hovers out of the div of the album, it will take out the class "opacity" from the HTMLL making the image of the album go back to the orginial picture. 
+//This function is in the divs of the 4 albums on "onmouseover" in the HTML. When the user hovers out of the div of the album, it will take out the class "opacity" from the HTML making the image of the album go back to the orginial picture. 
 function regular(elem){
 	elem.classList.remove("opacity");
 }	
